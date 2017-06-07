30a3bb6a9bd8f2221779101ae1041d651da8c63e
levimatheri <levimatheri@gmail.com>
2017-06-07 15:07:56 -0400

    added code to hide base option on change

--- avianMigration/web/script/change option.js
+++ avianMigration/web/script/change option.js
@@ -52,18 +52,23 @@ function inputOptionChange(evt) {
 //this will ensure a user cannot select an ending year/month/day that is less than the beginning year/month/day
 function selectOption(evt, ele)
 {
+    //hide --Please Select-- option   
+    $(".base").hide();
+    
     //make sure JS sees this as a number an not a string using the unary plus(+)
-    var begin = +(evt.target.value);
+    var begin = +(evt.target.value);   
     
     var elements = document.getElementsByClassName(ele.className);
     
     //get the end part of the variable i.e endYear or endMonth etc.
     var endOptions = document.getElementById(elements[1].id);
     
+    
     //set every option to default display at the beginning of each change
     for(var j = 0; j < endOptions.length; j++)
     {
-        endOptions[j].style="display:";
+        if(endOptions[j].className !== 'base')
+            endOptions[j].style="display:";
     }
     
     //loop through the options in end year and hide options that are less than the beginning year
@@ -72,12 +77,11 @@ function selectOption(evt, ele)
     {
         //make sure JS sees endOptions[i].value as a number an not a string using the unary plus(+)
         if(+endOptions[i].value < begin)
-        {
-            //console.log(+endOptions[i].value);
+        {            
             endOptions[i].style.display = "none";
         }
-        if(+endOptions[i].value === begin)
-            endOptions[i].selected = true;
+        if((+endOptions[i].value === begin))
+            endOptions[i].selected = true;        
     }
 }
 
